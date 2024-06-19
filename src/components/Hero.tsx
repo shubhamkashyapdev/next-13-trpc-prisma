ada96ea9915f79d3451dcb6b9697391e9c628bb4
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2024-06-19 23:48:36 +0530

    fix: github repo link added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,14 +22,18 @@ export function HomeHero() {
         }}
         className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
-        Next.js App Router With TRPC & Prisma{" "}
-        <Link className="relative" href="/">
+        Next.js App Router With TRPC & MongoDB Prisma{" "}
+        <Link
+          className="relative"
+          href="https://github.com/shubhamkashyapdev/next-trpc-prisma"
+          target="_blank"
+        >
           <ArrowUpRightSquareIcon className="inline absolute -top-2 -right-2 z-10 stroke-purple-600" />
           <Highlight className="text-black dark:text-white">
             Starter Template
           </Highlight>
         </Link>{" "}
-        To KickStart Your Next Full Stack Application
+        To KickStart Your Next Full Stack Application 🚀
       </motion.h1>
     </HeroHighlight>
   );
